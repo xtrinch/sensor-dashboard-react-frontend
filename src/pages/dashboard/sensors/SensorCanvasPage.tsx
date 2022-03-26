2410aeb9cf24528657c483aaee3f9111f5c570b0
xtrinch <mojca.rojko@gmail.com>
2022-03-26 11:56:17 +0100

    Inactive sensors to the right

--- src/pages/dashboard/sensors/SensorCanvasPage.tsx
+++ src/pages/dashboard/sensors/SensorCanvasPage.tsx
@@ -4,16 +4,18 @@ import { WithStyles } from '@mui/styles';
 import createStyles from '@mui/styles/createStyles';
 import withStyles from '@mui/styles/withStyles';
 import { SensorContext } from 'context/SensorContext';
-import React, { useContext, useEffect } from 'react';
-import MeasurementService from 'services/MeasurementService';
+import React, { useContext, useEffect, useMemo } from 'react';
 import ColorsEnum from 'types/ColorsEnum';
-import MeasurementTypeEnum from 'types/MeasurementTypeEnum';
-import Sensor from 'types/Sensor';
 import { observer } from 'mobx-react-lite';
 import SensorNow from './components/SensorNow';
 
 const styles = (theme) =>
   createStyles({
+    container: {
+      width: '100%',
+      display: 'flex',
+      flexDirection: 'row',
+    },
     root: {
       padding: '10px',
       display: 'grid',
@@ -24,6 +26,20 @@ const styles = (theme) =>
       width: '100%',
       boxSizing: 'border-box',
       overflow: 'auto',
+      marginRight: '200px',
+    },
+    rightbar: {
+      height: '100vh',
+      overflow: 'auto',
+      position: 'fixed',
+      right: '0',
+      top: '0',
+      width: '200px',
+      backgroundColor: ColorsEnum.BGLIGHTER,
+      padding: '10px 10px 0px 10px',
+      '& >div': {
+        marginBottom: '10px',
+      },
     },
   });
 
@@ -39,25 +55,35 @@ const SensorCanvasPage: React.FunctionComponent<WithStyles<typeof styles>> = (pr
     fetch();
   }, []);
 
+  const sortedSensors = useMemo(() => {
+    return sensorContext.mySensors.slice().sort((a, b) => {
+      if (!a.lastSeenAt) {
+        return 1;
+      }
+      if (isBefore(a.lastSeenAt, b.lastSeenAt)) {
+        return 1;
+      }
+      return -1;
+    });
+  }, [sensorContext.mySensors]);
+
   return (
-    <div style={{ width: '100%' }}>
+    <div className={classes.container}>
       {sensorContext.mySensors.length === 0 && (
         <Box style={{ textAlign: 'center', marginTop: '50px' }}>
           <Typography variant="h4">No sensors found. Try adding some.</Typography>
         </Box>
       )}
       <div className={classes.root}>
-        {sensorContext.mySensors
-          .slice()
-          .sort((a, b) => {
-            if (!a.lastSeenAt) {
-              return 1;
-            }
-            if (isBefore(a.lastSeenAt, b.lastSeenAt)) {
-              return 1;
-            }
-            return -1;
-          })
+        {sortedSensors
+          .filter((s) => s.lastMeasurements?.length)
+          .map((s) => (
+            <SensorNow sensor={s} key={s.id} />
+          ))}
+      </div>
+      <div className={classes.rightbar}>
+        {sortedSensors
+          .filter((s) => !s.lastMeasurements?.length)
           .map((s) => (
             <SensorNow sensor={s} key={s.id} />
           ))}
