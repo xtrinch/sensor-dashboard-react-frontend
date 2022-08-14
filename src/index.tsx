1a0a987a4b26dc2193d940f4bb964a1290054a21
xtrinch <mojca.rojko@gmail.com>
2022-08-14 12:10:05 +0200

    Add service worker registration

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 Sentry.init({
   dsn: config.sentryDSN,
 });
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+serviceWorkerRegistration.register();
